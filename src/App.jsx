343efe6c0640fe4a105a56e6d3876f971cb85e12
maetheartist <mariamkosemani1@gmail.com>
2024-07-05 23:52:08 +0100

    addedd my data.js file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <h1>hello world</h1>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,92 @@
+import picOne from "./images/chair1.png";
+import picTwo from "./images/chair2.png";
+import picThree from "./images/chair3.png";
+import picFour from "./images/chair4.png";
+import picFive from "./images/chair5.png";
+import picSix from "./images/chair6.png";
+import picSeven from "./images/chair7.png";
+import picEight from "./images/chair8.png";
+import picNine from "./images/chair9.png";
+import picTen from "./images/chair10.png";
+import picEleven from "./images/chair3.png";
+import picTwelve from "./images/chair4.png";
+
+export const allProducts = [
+  {
+    id: 1,
+    pic: picOne,
+    description:
+      "Atlas Extra Large Slipcover Modular Reversible Floor Sectional Sofa",
+    price: "$998",
+  },
+
+  {
+    id: 2,
+    pic: picTwo,
+    description: "Beryl Curved 3-Seater Couch Sofa and Lounge Chair by Acanva",
+    price: "$456 ",
+  },
+
+  {
+    id: 3,
+    pic: picThree,
+    description: "Persia Suedette 2-Piece Chaise Sectional Sofa and Armchair",
+    price: "$2050",
+  },
+
+  {
+    id: 4,
+    pic: picFour,
+    description: "Dori Boucle Curved 3-Seater Couch Sofa and Lounge Chair",
+    price: "$1280",
+  },
+  {
+    id: 5,
+    pic: picFive,
+    description: "Jell Armchair in Beige Fabric by Alter Ego Studio",
+    price: "$998",
+  },
+  {
+    id: 6,
+    pic: picSix,
+    description: "George Smith Kilim Standard Arm Sofa",
+    price: "$456",
+  },
+  {
+    id: 7,
+    pic: picSeven,
+    description: "Albert Park Demi Mid-Sofa",
+    price: "$2050",
+  },
+  {
+    id: 8,
+    pic: picEight,
+    description: " Corduroy Velvet Camaleonda Modular Sofa",
+    price: "$1280",
+  },
+  {
+    id: 9,
+    pic: picNine,
+    description:
+      "Atlas Extra Large Slipcover Modular Reversible Floor Sectional Sofa",
+    price: "$998",
+  },
+  {
+    id: 10,
+    pic: picTen,
+    description: "Beryl Curved 3-Seater Couch Sofa and Lounge Chair by Acanva",
+    price: "$456",
+  },
+  {
+    id: 11,
+    pic: picEleven,
+    description: "Persia Suedette 2-Piece Chaise Sectional Sofa and Armchair",
+    price: "$2050",
+  },
+  {
+    id: 12,
+    pic: picTwelve,
+    description: "Dori Boucle Curved 3-Seater Couch Sofa and Lounge Chair",
+    price: "$1280",
+  },
+];
